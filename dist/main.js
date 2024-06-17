b956497ed8e4fac9dec99f64429ef9d1ccd0a2a2
Akniet <kuluev877@gmail.com>
2024-06-17 15:54:23 +0600

    commit in heroku

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
 const PORT = 3000;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Kanban')
+        .setDescription('The kanban API description')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('', app, document);
     await app.listen(PORT);
     console.log(`http://localhost:${PORT}`);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 const PORT = 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Kanban')
+    .setDescription('The kanban API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
   console.log(`http://localhost:${PORT}`)
 }
