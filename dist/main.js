4f5a72b9bd2f37a7d8093e8dd9f9eee947ed749a
Akniet <kuluev877@gmail.com>
2024-07-05 16:30:42 +0600

    commit

--- dist/main.js
+++ dist/main.js
@@ -13,7 +13,7 @@ async function bootstrap() {
         .addBearerAuth()
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
-    swagger_1.SwaggerModule.setup('', app, document);
+    swagger_1.SwaggerModule.setup('api', app, document);
     await app.listen(PORT);
     console.log(`http://localhost:${PORT}`);
 }
